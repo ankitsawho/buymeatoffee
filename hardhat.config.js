c7dcfdd92dd8553c6a0a691644735979552cea15
Ankit <ankit1042002@gmail.com>
2023-09-01 09:04:22 +0530

    changed network to mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,17 +2,29 @@ require("@nomicfoundation/hardhat-toolbox");
 
 require("dotenv").config();
 
-const SEPOLIA_URL = process.env.SEPOLIA_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-
 /** @type import('hardhat/config').HardhatUserConfig */
+const { POLYGON_MUMBAI_RPC_PROVIDER, PRIVATE_KEY, POLYGONSCAN_API_KEY } =
+	process.env;
+
 module.exports = {
 	solidity: "0.8.18",
 	networks: {
-		sepolia: {
-			url: SEPOLIA_URL,
-			accounts: [PRIVATE_KEY],
+		mumbai: {
+			url: POLYGON_MUMBAI_RPC_PROVIDER,
+			accounts: [`0x${PRIVATE_KEY}`],
 		},
+	},
+	solidity: {
+		version: "0.8.19",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
+	etherscan: {
+		apiKey: POLYGONSCAN_API_KEY,
 	},
 };
 
